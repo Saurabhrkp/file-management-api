8826c0c2947c71777031aa0721ee38e4835606ee
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-01-07 03:01:45 +0530

    feat(route): adds new file route to router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import users from './users/route';
+import files from './files/route';
+import authentication from 'auth/authentication';
 
 const router = Router();
 
 router.use('/users', users);
+
+router.use('/files', authentication, files);
 
 export default router;
