86e8ecd75a4205efc995e52237bd1918093d3dfd
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-01-06 16:10:52 +0530

    feat(middleware): adds middleware for app

---
+++ src/middlewares/apiLogger.ts
@@ -0,0 +1,45 @@
+import { transports, format } from 'winston'
+import expressWinston from 'express-winston'
+
+export const apiLogger = expressWinston.logger({
+	transports: [new transports.Console()],
+	format: format.combine(
+		format.errors({ stack: true }),
+		format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+		format.printf(({ level, message, timestamp, stack }) => {
+			if (stack) return `${timestamp} ${level}: ${message}\n${stack}` // Custom format for error logs
+			return `${timestamp} ${level}: ${message}` // Default format for other logs
+		})
+	),
+	meta: false,
+	expressFormat: true,
+	colorize: false,
+	statusLevels: true,
+	skip: (req, _res) => req.method === 'OPTIONS',
+})
+
+export const errorLogger = expressWinston.errorLogger({
+	transports: [new transports.Console()],
+	format: format.combine(
+		format.errors({ stack: true }),
+		format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+		format.printf(({ level, message, timestamp, meta }) => {
+			const error = JSON.stringify(meta?.error)
+			const username = JSON.stringify(meta?.username)
+			console.error({ timestamp, level, message, meta })
+			return `${timestamp} ${level}: ${message}, username: ${username}, error: ${error}`
+		})
+	),
+	meta: true,
+	requestField: null,
+	blacklistedMetaFields: ['process', 'stack', 'trace', 'os', 'message'],
+	dynamicMeta: (req, res, _err) => {
+		return {
+			url: req.url,
+			body: req.body,
+			username: res.locals?.payload?.username,
+			tokenId: res.locals?.payload?.tokenId,
+		}
+	},
+	skip: (req, _res) => req.method === 'OPTIONS',
+})
---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+import { AccessTokenError, AuthFailureError } from 'helpers/errors'
+import { JwtPayload } from 'helpers/jwt'
+
+declare global {
+	namespace Express {
+		interface Request {
+			headers?: any
+			payload?: JwtPayload
+		}
+	}
+}
+
+export const authentication =
+	(publicKey: string) =>
+	async (req: Request, res: Response, next: NextFunction) => {
+		if (!req.headers['x-access-token'])
+			throw new AuthFailureError('Unable to find access token.')
+		if (!req.headers['x-refresh-token'])
+			throw new AuthFailureError('Unable to find refresh token.')
+
+		const payload = verify(
+			req.headers['x-access-token'].toString(),
+			publicKey
+		) as JwtPayload
+
+		if (!payload || !payload.iss || !payload.aud || !payload.prm)
+			throw new AccessTokenError('Invalid Access Token')
+
+		res.locals.payload = payload
+
+		return next()
+	}
