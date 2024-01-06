fccdbe2723b230e26e1fb111094377c88a710ec4
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-01-06 20:25:00 +0530

    fix(typo): fixes typo for limit file size in mb

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -16,7 +16,7 @@ export const uploadMediaHandler = (feildNames) => (req, res, next) => {
 
   const uploadHandler = multer({
     storage: storage,
-    limits: { fileSize: SIZES.MEDIA_IN_MB * SIZES.BYTES_IN_KILOBYTE * SIZES.KILOBYTES_IN_MEGABYTE },
+    limits: { fileSize: SIZES.FILE_IN_MB * SIZES.BYTES_IN_KILOBYTE * SIZES.KILOBYTES_IN_MEGABYTE },
     fileFilter: (_req, file, next) => {
       const notAllowedMimetypes = ['application/x-msdownload', 'application/x-dosexec', 'application/x-executable', 'application/x-macbinary', 'application/x-sh', 'application/x-bat', 'application/x-msdos-program', 'application/java-archive', 'application/x-php', 'application/x-perl'];
       (!notAllowedMimetypes.includes(file.mimetype)) ? next(null, true) : next(`Invalid file type: ${file.mimetype}. Only audio/video/image/pdf/doc/xlsx files are allowed.`);
